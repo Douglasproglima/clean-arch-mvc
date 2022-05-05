150e0b15dbe99733124e05036a9f10a4d5c446aa
Douglas Lima <Douglasproglima@gmail.com>
2022-05-04 21:50:08 -0300

    Add anemic entities in Domain

---
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Domain
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+    }
+}
