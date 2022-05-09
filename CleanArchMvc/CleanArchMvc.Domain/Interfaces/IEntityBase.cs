8fcd09b3de28f26d770148f2c522cc33cdd075a0
Douglas Lima <Douglasproglima@gmail.com>
2022-05-09 20:28:18 -0300

    Add Interface Pattern Repository

---
+++ CleanArchMvc/CleanArchMvc.Domain/Interfaces/IEntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Domain.Interfaces
+{
+    internal interface IEntityBase<T>
+    {
+        Task<IEnumerable<T>> GetAll();
+        Task<T> GetById(int? id);
+        Task<T> Create(T entity);
+        Task<T> Update(T entity);
+        Task<T> Remove(T entity);
+    }
+}
