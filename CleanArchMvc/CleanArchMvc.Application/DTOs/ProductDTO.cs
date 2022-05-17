92cf1f1566a07cea167ac4603156c55bae299024
Douglas Lima <Douglasproglima@gmail.com>
2022-05-16 23:12:57 -0300

    Add DTO's Category and Product

---
+++ CleanArchMvc/CleanArchMvc.Application/DTOs/ProductDTO.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Domain.Entities;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CleanArchMvc.Application.DTOs
+{
+    public class ProductDTO
+    {
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        [DisplayName("Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório.")]
+        [MinLength(5)]
+        [MaxLength(200)]
+        [DisplayName("Description")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "O campo Preço é obrigatório.")]
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Price")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "O campo Estoque é obrigatório.")]
+        [Range(1, 9999)]
+        [DisplayName("Stock")]
+        public int Stock { get; set; }
+
+        [MaxLength(250)]
+        [DisplayName("Product Image")]
+        public string Image { get; set; }
+
+        public Category Category { get; set; }
+
+        [DisplayName("Categories")]
+        public int CategoryId { get; set; }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Application/Mappings/DomainToDTOMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Domain.Entities;
+
+namespace CleanArchMvc.Application.Mappgins
+{
+    public class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}
