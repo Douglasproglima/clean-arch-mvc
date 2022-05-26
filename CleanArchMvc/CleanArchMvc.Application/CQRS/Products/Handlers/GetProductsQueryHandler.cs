5617a451e3a07258317f517f638bfaf472ba9b29
Douglas Lima <Douglasproglima@gmail.com>
2022-05-25 21:11:15 -0300

    Create Product Handlers

---
+++ CleanArchMvc/CleanArchMvc.Application/CQRS/Products/Handlers/GetProductsQueryHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Application.CQRS.Products.Queries;
+
+namespace CleanArchMvc.Application.CQRS.Products.Handlers
+{
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, IEnumerable<Product>>
+    {
+        #region Propertits/Attributes
+        private readonly IProductRepository _productRepository;
+        #endregion
+
+        #region Constructor
+        public GetProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentException(nameof(productRepository));
+        }
+        #endregion
+
+        #region Methods
+        public async Task<IEnumerable<Product>> Handle(
+            GetProductsQuery request,
+            CancellationToken cancellationToken)
+        {
+            return await _productRepository.GetProductsAsync();
+        }
+        #endregion
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.Application/CQRS/Products/Handlers/ProductUpdateCommandHandler.cs
@@ -0,0 +1,48 @@
+﻿using CleanArchMvc.Application.CQRS.Products.Commands;
+using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.CQRS.Products.Handlers
+{
+    public class ProductUpdateCommandHandler : IRequestHandler<ProductUpdateCommand, Product>
+    {
+        #region Propertits/Attributes
+        private readonly IProductRepository _productRepository;
+        #endregion
+
+        #region Constructor
+        public ProductUpdateCommandHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository ??
+                throw new ArgumentException(nameof(productRepository));
+        }
+        #endregion
+
+        #region Methods
+        public async Task<Product> Handle(
+            ProductUpdateCommand request,
+            CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product == null)
+                throw new ApplicationException($"Erro ao atualizar entidade!");
+
+            product.Update(
+                request.CategoryId,
+                request.Name,
+                request.Description,
+                request.Price,
+                request.Stock,
+                request.Image
+            );
+
+            return await _productRepository.UpdateAsync(product);
+        }
+        #endregion
+    }
+}
