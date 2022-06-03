0cc72085d241b2e73a172be661624ce639b6db30
Douglas Lima <Douglasproglima@gmail.com>
2022-06-02 22:54:32 -0300

    Implementation concret class AuthenticateService

---
+++ CleanArchMvc/CleanArchMvc.Infra.Data/Identity/AuthenticateService.cs
@@ -0,0 +1,52 @@
+﻿using CleanArchMvc.Domain.Interfaces.Account;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Identity
+{
+    public class AuthenticateService : IAuthenticate
+    {
+        #region Atributoss/Propriedades
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        #endregion
+
+        #region Construtor
+        public AuthenticateService(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        #endregion
+
+        #region Métodos
+        public async Task<bool> Authenticate(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+
+            return result.Succeeded;
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var applicationUser = new ApplicationUser
+            {
+                UserName = email,
+                Email = email
+            };
+
+            var result = await _userManager.CreateAsync(applicationUser, password);
+
+            if(result.Succeeded)
+                await _signInManager.SignInAsync(applicationUser, isPersistent: false);
+
+            return result.Succeeded;
+        }
+
+        public async Task Logout()
+        {
+            await _signInManager.SignOutAsync();
+        }
+        #endregion
+    }
+}
