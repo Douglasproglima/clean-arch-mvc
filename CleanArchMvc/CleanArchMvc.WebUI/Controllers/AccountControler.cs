288b3edd7bea9b7c6d0a01dd53cce1f4ee66c6c7
Douglas Lima <Douglasproglima@gmail.com>
2022-06-07 20:53:02 -0300

    Start implementation Obj Account

---
+++ CleanArchMvc/CleanArchMvc.WebUI/Controllers/AccountControler.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchMvc.Domain.Interfaces.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountControler : Controller
+    {
+        #region Atributos/Propriedades
+        private readonly IAuthenticate _authenticate;
+        #endregion
+
+        #region Construtor
+        public AccountControler(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate;
+        }
+        #endregion
+
+        #region
+        #endregion
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CleanArchMvc/CleanArchMvc.WebUI/ViewModels/RegisterViewDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.WebUI.ViewModels
+{
+    public class RegisterViewDTO
+    {
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatório!")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Repita a Senha")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Senha é diferente.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
