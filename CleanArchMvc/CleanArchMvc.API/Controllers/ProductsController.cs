e4af92706763d2d462811c51ef391e3d8d2486f5
Douglas Lima <Douglasproglima@gmail.com>
2022-06-09 23:12:30 -0300

    Version API Products

--- CleanArchMvc/CleanArchMvc.API/Controllers/ProductsController.cs
+++ CleanArchMvc/CleanArchMvc.API/Controllers/ProductsController.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArchMvc.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
