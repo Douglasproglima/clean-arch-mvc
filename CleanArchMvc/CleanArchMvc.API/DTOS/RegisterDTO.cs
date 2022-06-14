77e9193cd633ac4143b2dfc127234f0a2cb6b515
Douglas Lima <Douglasproglima@gmail.com>
2022-06-13 22:40:09 -0300

    prepare DTO's JWT

---
+++ CleanArchMvc/CleanArchMvc.API/DTOS/RegisterDTO.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CleanArchMvc.API.DTOS
+{
+    public class RegisterDTO
+    {
+        [Required(ErrorMessage = "E-mail é obrigatório!")]
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha é obrigatório!")]
+        [StringLength(20, ErrorMessage = "A senha deve ter no mínimo {0} e no máximo {1} caracteres.", MinimumLength = 10)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
