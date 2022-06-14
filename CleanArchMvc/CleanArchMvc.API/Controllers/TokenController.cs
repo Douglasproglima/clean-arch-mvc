28780b857172e153174e9081c74fe281414deb01
Douglas Lima <Douglasproglima@gmail.com>
2022-06-13 22:56:58 -0300

    Add Token Controller

---
+++ CleanArchMvc/CleanArchMvc.API/Controllers/TokenController.cs
@@ -0,0 +1,44 @@
+﻿using CleanArchMvc.API.DTOS;
+using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        #region Atributos/Propriedades
+        private readonly IAuthenticate _authenticate;
+        #endregion
+        
+        #region Construtor
+        public TokenController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate ?? 
+                throw new ArgumentNullException(nameof(authenticate));
+        }
+        #endregion
+
+        #region Métodos Login
+        [HttpPost("LoginUser")]
+        public async Task<ActionResult<UserToken>> Login([FromBody] LoginDTO userInfo)
+        {
+            var result = await _authenticate.Authenticate(userInfo.Email, userInfo.Password);
+            if (result)
+            { 
+                return Ok($"User: { userInfo } - Login realizado com sucesso.");
+            //return GenarateToken(userInfo);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Falha ao realizar login!");
+                return BadRequest(ModelState);
+            }
+        }
+        #endregion
+    }
+}
